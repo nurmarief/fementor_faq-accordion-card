4f893e88100dd1d2ca58459c69410c67206bf847
nurmarief <ariefmuhammad897@gmail.com>
2023-10-19 11:14:43 +0700

    create images dir

---
+++ src/images/index.js
@@ -0,0 +1,7 @@
+import './bg-pattern-desktop.svg';
+import './bg-pattern-mobile.svg';
+import './favicon-32x32.png';
+import './icon-arrow-down.svg';
+import './illustration-box-desktop.svg';
+import './illustration-woman-online-desktop.svg';
+import './illustration-woman-online-mobile.svg';
